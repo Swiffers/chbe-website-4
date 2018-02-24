671179b0bd0f3c6088f2896221a6bc795c95a9b6
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2018-02-24 15:37:48 +0100

    Intro section with typed and SVG logo

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function() {
+    var typed = new Typed('#typed-render', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 40,
+        backSpeed: 20,
+        smartBackspace: true,
+        backDelay: 1000,
+        loop: true,
+        loopCount: Infinity,
+        showCursor: false
+    });
+})();
